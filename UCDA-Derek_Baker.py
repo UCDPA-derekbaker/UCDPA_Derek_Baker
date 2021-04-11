f2e9e0044989b0307b41b78f6678eeae1531458d
UCDPA-derekbaker <derek.baker@concern.net>
2021-04-11 15:57:24 +0100

    Created a dataframe with statistical information about the vaccinations per country

--- UCDA-Derek_Baker.py
+++ UCDA-Derek_Baker.py
@@ -17,11 +17,6 @@ print(cv_df.describe())  # Get summary stats for each column
 print(type(cv_df)) # This is a pandas dataframe
 print(cv_df.values)  # Prints a 2 dimensional Numpy array of values
 
-# # Having looked at the data, there is a date column to sort by as the data is already sorted by country and date so I'll set the index by these two columns merged as a new column
-# # I prefer the index ordered by country so I won't change it or set it descending
-# cv_df = cv_df.set_index("date").sort_index()
-
-
 # Delete columns not needed for this analysis
 # If all the columns to delete were the last columns, I would have used a slice command cv_df = cv_df.loc[:,:"vaccines"]
 cv_df.drop(["total_vaccinations", "people_vaccinated", "people_fully_vaccinated", "total_vaccinations_per_hundred",
@@ -31,19 +26,18 @@ cv_df.drop(["total_vaccinations", "people_vaccinated", "people_fully_vaccinated"
 # Check that the columns have been removed
 print(cv_df.columns)
 
-# Look ti find rows with missing data - count the number of cells with null values
+# Look to find rows with missing data - count the number of cells with null values
 cv_df.isna().sum()
 
 # There are two columns (daily_vaccinations + daily_vaccinations_per_million) both with 187 rows with missing data
 # and the other columns don't have any value without this data so I'll remove the rows instead of replacing the values with cv_df.fillna(0)
 cv_df = cv_df.dropna()
-
 # Double check for any missing values
 cv_df.isna().sum()
 
 # Find out how many vaccines are given daily to all countries
 total_daily_vac = cv_df.groupby("date")["daily_vaccinations"].sum()
-print(total_daily_vac) # Not sure if the numbers are balancing will investigate in the next session
+print(total_daily_vac) # Data is accurate
 
 # Find out how many days each country has received vaccinations
 cv_df_group_daily_vacs = cv_df[['country', 'daily_vaccinations']].groupby("country")
